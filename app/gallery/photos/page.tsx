d31288e207fd922bd63c76dc5fd0345f1dcab551
beatychemtai01 <beatychemtai01@gmail.com>
2026-06-01 11:26:27 +0300

    Update page.tsx

--- app/gallery/photos/page.tsx
+++ app/gallery/photos/page.tsx
@@ -55,6 +55,7 @@ const photos = [
   { src: "/assets/photos/photo39.jpeg", alt: "" },
   { src: "/assets/photos/photo40.jpeg", alt: "" },
   { src: "/assets/photos/photo41.jpeg", alt: "" },
+    { src: "/assets/photos/photo42.jpeg", alt: "" },
   { src: "/assets/photos/logo.png", alt: "" },
   
 
